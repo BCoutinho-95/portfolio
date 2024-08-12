490a5da5689a472dfd4808d49603a8096fe766c2
Bruno Coutinho <bmferreira298@gmail.com>
2024-08-12 12:41:05 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/BCoutinho-95.png" />
+    <Titulo fontSize={20}>Bruno Coutinho</Titulo>
+  </aside>
+)
+
+export default Sidebar
